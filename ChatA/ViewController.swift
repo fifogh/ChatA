1bd0e0beb1ea801530c68fbc2397e396634c1ac5
Philippe Faurie <phfaurie@gmail.com>
2020-03-10 10:35:46 -0700

    Initial Commit

---
+++ ChatA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatA
+//
+//  Created by Philippe Faurie on 3/10/20.
+//  Copyright © 2020 Philippe Faurie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
