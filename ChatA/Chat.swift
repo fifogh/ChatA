043771c064c95ed3cfa78faa6f309fceeac6b2f2
Philippe Faurie <phfaurie@gmail.com>
2020-03-21 18:31:34 -0700

    First commit

---
+++ ChatA/Chat.swift
@@ -0,0 +1,18 @@
+//
+//  Chat.swift
+//  ChatA
+//
+//  Created by Philippe Faurie on 3/18/20.
+//  Copyright © 2020 Philippe Faurie. All rights reserved.
+//
+
+import Foundation
+
+class ChatAnalyzer {
+    
+    func printChat () {
+        
+        print ("Chat Numnber :")
+        print (chatL.count)
+    }
+}
